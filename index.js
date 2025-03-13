95ac462987811b3f0c78269c49b37a4083f2acb3
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-13 17:44:34 +0100

    feat: images added

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const token = process.env.DISCORD_TOKEN;
-client.login(token);
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
+const dotenv = require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+    ],
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+    console.log(`Depende si no gomito ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
 });
 
