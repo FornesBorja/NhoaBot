58d5a5eba2405cbc54097b8af3df2ae1dec21a1e
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-13 11:28:29 +0100

    feat: basic token login discord

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = process.env.DISCORD_TOKEN;
+client.login(token);
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
