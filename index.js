b0939838c13e74bdf4380516ea2e6e84554bbaa8
Esperanza Fornes <fornes.borja@gmail.com>
2025-03-14 04:44:51 +0100

    feat: bot now can reproduce yt videos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const { DisTube } = require('distube');
 const dotenv = require("dotenv").config();
-const {Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const { SpotifyPlugin } = require('@distube/spotify');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
+const ytdl = require('ytdl-core');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 process.env.FFMPEG_PATH = ffmpeg.path;
 
@@ -18,11 +20,15 @@ const client = new Client({
 
 const distube = new DisTube(client, { 
     emitNewSongOnly: true,
-    plugins: [new SpotifyPlugin()]
+    plugins: [
+        new SpotifyPlugin(),
+        new YtDlpPlugin()
+    ],
 });
 
 client.on('messageCreate', async (message) => {
     if (!message.content.startsWith('.play')) return;
+
     const args = message.content.split(' ').slice(1);
     if (!message.member.voice.channel) {
         return message.reply('¡Debes estar en un canal de voz!');
@@ -31,12 +37,26 @@ client.on('messageCreate', async (message) => {
     const query = args.join(' ');
     if (!query) return message.reply('Debes escribir el nombre de una canción o un enlace.');
 
-    distube.play(message.member.voice.channel, query, {
-        textChannel: message.channel,
-        member: message.member,
-    });
+    try {
+        
+        if (ytdl.validateURL(query)) {
+            message.reply('Reproduciendo canción: ' + query);
+            await distube.play(message.member.voice.channel, query, {
+                textChannel: message.channel,
+                member: message.member,
+            });
+        } else {
+            console.log('Buscando canción: ' + query);
+            await distube.play(message.member.voice.channel, query, {
+                textChannel: message.channel,
+                member: message.member,
+            });
+        }
+    } catch (error) {
+        console.error('Error al intentar reproducir la canción:', error);
+        return message.reply('Hubo un error al intentar reproducir la canción. Intenta de nuevo más tarde.');
+    }
 });
-
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -49,4 +69,3 @@ client.on('messageCreate', (message) => {
         message.reply('pong');
     }
 });
-
