7c9e0945a046796ef991be356bc73fbe6f192c95
Esperanza Fornes <fornes.borja@gmail.com>
2025-04-04 22:15:07 +0200

    feat: now wait before leaving

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -4,19 +4,25 @@ module.exports = {
     name: 'leave',
     async execute(message) {
         const queue = distube.getQueue(message.guild.id);
-        
+
         if (!message.member.voice.channel) {
             return message.reply('¡Debes estar en un canal de voz!');
         }
 
         if (queue) {
-            queue.stop();
-            queue.voice.leave();
-            return message.channel.send('👋 ¡Adiós!');
+            setTimeout(() => {
+                queue.stop();
+                queue.voice.leave();
+                return message.channel.send('👋 ¡Adiós!');
+            }, 60000);
+
+
         } else {
             if (message.guild.members.me.voice.channel) {
-                message.guild.members.me.voice.disconnect();
-                return message.channel.send('👋 ¡Adiós!');
+                setTimeout(() => {
+                    message.guild.members.me.voice.disconnect();
+                    return message.channel.send('👋 ¡Adiós!');
+                }, 60000);
             } else {
                 return message.reply('¡No estoy en ningún canal de voz!');
             }
